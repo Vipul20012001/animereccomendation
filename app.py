7095ec8bb006aa06865bd10b1fe92c1218aa5664
Vipul20012001 <59278273+Vipul20012001@users.noreply.github.com>
2024-02-10 19:29:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+INPUT_DIR = 'archive'
+rating_df = pd.read_csv('archive/animelist.csv',
+                       usecols=["user_id", "anime_id", "rating"]
+                        #, nrows=90000000
+                        )
+print(rating_df.head(4))
+n_ratings = rating_df['user_id'].value_counts()
+rating_df = rating_df[rating_df['user_id'].isin(n_ratings[n_ratings >= 400].index)].copy()
+len(rating_df)
+min_rating = min(rating_df['rating'])
+max_rating = max(rating_df['rating'])
+rating_df['rating'] = rating_df["rating"].apply(lambda x: (x - min_rating) / (max_rating - min_rating)).values.astype(np.float64)
+
+AvgRating = np.mean(rating_df['rating'])
+print('Avg', AvgRating)
+duplicates = rating_df.duplicated()
+
+if duplicates.sum() > 0:
+    print('> {} duplicates'.format(duplicates.sum()))
+    rating_df = rating_df[~duplicates]
+
+print('> {} duplicates'.format(rating_df.duplicated().sum()))
+g = rating_df.groupby('user_id')['rating'].count()
+top_users = g.dropna().sort_values(ascending=False)[:20]
+top_r = rating_df.join(top_users, rsuffix='_r', how='inner', on='user_id')
+
+g = rating_df.groupby('anime_id')['rating'].count()
+top_animes = g.dropna().sort_values(ascending=False)[:20]
+top_r = top_r.join(top_animes, rsuffix='_r', how='inner', on='anime_id')
+
+pd.crosstab(top_r.user_id, top_r.anime_id, top_r.rating, aggfunc=np.sum)
+user_ids = rating_df["user_id"].unique().tolist()
+user2user_encoded = {x: i for i, x in enumerate(user_ids)}
+user_encoded2user = {i: x for i, x in enumerate(user_ids)}
+rating_df["user"] = rating_df["user_id"].map(user2user_encoded)
+n_users = len(user2user_encoded)
+
+anime_ids = rating_df["anime_id"].unique().tolist()
+anime2anime_encoded = {x: i for i, x in enumerate(anime_ids)}
+anime_encoded2anime = {i: x for i, x in enumerate(anime_ids)}
+rating_df["anime"] = rating_df["anime_id"].map(anime2anime_encoded)
+n_animes = len(anime2anime_encoded)
+
+print("Num of users: {}, Num of animes: {}".format(n_users, n_animes))
+print("Min rating: {}, Max rating: {}".format(min(rating_df['rating']), max(rating_df['rating'])))
+rating_df = rating_df.sample(frac=1, random_state=73)
+
+X = rating_df[['user', 'anime']].values
+y = rating_df["rating"]
+test_set_size = 10000 #10k for test set
+train_indices = rating_df.shape[0] - test_set_size 
+
+X_train, X_test, y_train, y_test = (
+    X[:train_indices],
+    X[train_indices:],
+    y[:train_indices],
+    y[train_indices:],
+)
+
+print('> Train set ratings: {}'.format(len(y_train)))
+print('> Test set ratings: {}'.format(len(y_test)))
